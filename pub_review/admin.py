1fb0a6536b022cde18253d873b031669fe9f0158
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2024-03-07 21:57:29 +0000

    registered models

--- pub_review/admin.py
+++ pub_review/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from pub_review.models import *
 
 # Register your models here.
+admin.site.register(UserTable)
+admin.site.register(PubTable)
+admin.site.register(Top5_PubTable)
+admin.site.register(AnswerTable)
+admin.site.register(QuestionTable)
+admin.site.register(ReviewTable)
+
+
