28659dfc7bc5df9de1ac79db6ee5aee1f8639c22
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2024-03-07 17:03:29 +0000

    fixed imports

--- pub_review/views.py
+++ pub_review/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from pub_review.models import Pub, Review, Question, Answer, User
 
 # Create your views here.
 def index(request):
