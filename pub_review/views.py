2224ac0be30af7ab829cbbc718c1efce96d36ace
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2024-03-07 17:53:49 +0000

    added some basic functions which call forms These forms still need to be made and the functions configured around them.

--- pub_review/views.py
+++ pub_review/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from pub_review.models import Pub, Review, Question, Answer, User
+from pub_review.forms import AddPubForm, UserForm, UserProfileForm, EditPubForm, AddAnswerForm, EditAnswerFrom, ReviewForm, EditReviewForm
 
 # Create your views here.
 def index(request):
@@ -25,18 +29,53 @@ def search(request):
     return render(request, 'pub_review/index.html', context= context_dict)
 
 def pubs(request):
+    context_dict = {}
+    # get list of all pubs?
+    
     return HttpResponse("<h1>To be made</h1>")
 
+@login_required     # this should maybe be admin only?
 def add_pub(request):
-    return HttpResponse("<h1>To be made</h1>")
+    # make call to AddPub form
+    form = AddPubForm()
+    
+    if request.method == "POST":
+        form = AddPubForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            #TO-DO
+            return redirect()               # Not sure if this should become a redirect to the newly created pub page?
+        else:
+            print(form.errors)
+
+    return render(request, 'pub_review/pubs/add_pub.html', {'form':form})
 
 def show_pub(request):
     return HttpResponse("<h1>To be made</h1>")
 
+@login_required
 def edit_pub(request):
+        # make call to AddPub form
+    form = EditPubForm()
+    
+    if request.method == "POST":
+        form = EditPubForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            #TO-DO
+            return redirect()               # redirect to newly edited pub page.
+        else:
+            print(form.errors)
+
     return HttpResponse("<h1>To be made</h1>")
 
 def show_reviews(request):
+    return HttpResponse("<h1>To be made</h1>")
+
+@login_required # maybe can be done by hiding like button, instead of this way.
+def like_review_view(request):
+    # This will be JS function to add the like button functionality to the 
+    
     return HttpResponse("<h1>To be made</h1>")
 
 def show_review(request):
@@ -48,9 +87,11 @@ def list_questions(request):
 def show_question(request):
     return HttpResponse("<h1>To be made</h1>")
 
+@login_required
 def add_answer(request):
     return HttpResponse("<h1>To be made</h1>")
 
+@login_required
 def edit_answer(request):
     return HttpResponse("<h1>To be made</h1>")
 
@@ -60,6 +101,7 @@ def user_profile(request):
 def user_profile_reviews(request):
     return HttpResponse("<h1>To be made</h1>")
 
+@login_required
 def edit_user_profile(request):
     return HttpResponse("<h1>To be made</h1>")
 
@@ -67,7 +109,36 @@ def user_login(request):
     return HttpResponse("<h1>To be made</h1>")
 
 def register(request):
-    return HttpResponse("<h1>To be made</h1>")
+    #TO-DO finish configuring the user forms, this is not suitable for our use.
+    registered = False
 
+    if request.method == 'POST':
+        # most of this sourced from tango with django online tutorial
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    
+    return render(request, 'pub_review/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+@login_required
 def user_logout(request):
-    return HttpResponse("<h1>To be made</h1>")
+    logout(request)
+    return redirect(reverse('rango:index'))
