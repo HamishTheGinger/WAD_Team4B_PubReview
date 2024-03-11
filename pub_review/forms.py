8835babf8febc43fd6bb9a39fccad75c47d38039
Taewan Kim <2798186k@student.gla.ac.uk>
2024-03-12 05:25:46 +0900

    second commit

--- pub_review/forms.py
+++ pub_review/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pub_review.models import Question, Answer, UserProfile
+from pub_review.models import Question, Answer, UserProfile,Pub,Review,Pub_Answer,Pub_Question,FavoritePubs
 from django.contrib.auth.models import User
 class QuestionForm(forms.ModelForm):
     class Meta:
@@ -19,10 +19,18 @@ class AnswerForm(forms.ModelForm):
         }
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password1 = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Comfirmation',widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields =('username','password',)
+        fields =('username','email','password1','password2')
+        labels = {
+            'username': 'User_name',
+            'email': 'Email',
+            'password1': 'Password',
+            'password2': 'Confirmation',
+        }
+
 
 
 class UserProfileForm(forms.ModelForm):
@@ -31,10 +39,42 @@ class UserProfileForm(forms.ModelForm):
         fields = ('picture','firstName','lastName','sex','age','nationality',)
 
         labels = {
-            'picture': 'User Image',
-            'firstName': 'First Name',
-            'lastName': 'Last Name',
+            'picture': 'User_Image',
+            'firstName': 'First_Name',
+            'lastName': 'Last_Name',
             'sex': 'Sex',
             'age': 'Age',
             'nationality': 'Nationality'
         }
+
+class PubProfileForm(forms.ModelForm):
+    class Meta:
+        model = Pub
+        fields = ('pubName','city','streetName','postcode','picture')
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['subject','content','picture']
+
+class Pub_QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Pub_Question
+        fields = ['subject','content']
+        labels ={
+            'subject':'Title',
+            'content':'Content',
+        }
+
+class Pub_AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Pub_Answer
+        fields = ['content']
+        label ={
+            "content": 'Answer',
+        }
+
+class FavoritePubForm(forms.ModelForm):
+    class Meta:
+        model = FavoritePubs
+        fields = ['pub1','pub2','pub3','pub4','pub5']
--- pub_review/models.py
+++ pub_review/models.py
@@ -6,37 +6,79 @@ class Question(models.Model):
     subject = models.CharField(max_length =200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
 
 class UserProfile(models.Model):
     # These are commented out as they are stored in the django models.User table
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='profile_images',blank= True)
-    firstName = models.CharField(max_length=128)
-    lastName = models.CharField(max_length=128)
-    sex = models.CharField(max_length=10,blank= True)
-    age = models.IntegerField(default=0,blank= True)
-    nationality = models.CharField(max_length=40,blank= True)
+    picture = models.ImageField(upload_to='userProfile_images',blank= True,null=True)
+    firstName = models.CharField(max_length=128,blank= True,null=True)
+    lastName = models.CharField(max_length=128,blank=True,null=True)
+    sex = models.CharField(max_length=10,blank=True,null=True)
+    age = models.CharField(max_length=10,blank=True,null=True)
+    nationality = models.CharField(max_length=40,blank=True,null=True)
 
     def __str__(self):
         return self.user.username
 
 class Pub(models.Model):
-    Pub_Name = models.CharField(max_length=128)
-    City = models.CharField(max_length=128)
-    Street_Name = models.CharField(max_length=128)
-    Postcode = models.CharField(max_length=7) # 7 chars long, longest postcode is "AB123YZ" format
-    UserID = models.ForeignKey(User, on_delete = models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,related_name='owner_pub')
+    pubName = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    streetName = models.CharField(max_length=128)
+    postcode = models.CharField(max_length=7) # 7 chars long, longest postcode is "AB123YZ" format
+    picture = models.ImageField(upload_to='pubProfile_images', blank=True,null=True)
+    voter = models.ManyToManyField(User,related_name='voter_pub')
+    def __str__(self):
+        return self.pubName
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    pub = models.ForeignKey(Pub,on_delete=models.CASCADE)
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    picture = models.ImageField(upload_to='review_images', blank=True,null=True)
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return self.Pub_Name
+        return self.subject
 
+class Pub_Question(models.Model):
+    subject = models.CharField(max_length =200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    pub = models.ForeignKey(Pub,on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
+    def __str__(self):
+        return self.subject
+
+class Pub_Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    question = models.ForeignKey(Pub_Question, on_delete = models.CASCADE)
+    content = models.TextField()
+    pub = models.ForeignKey(Pub, on_delete=models.CASCADE)
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+
+class FavoritePubs(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    pub1 = models.CharField(max_length=100,null=True, blank=True)
+    pub2 = models.CharField(max_length=100,null=True, blank=True)
+    pub3 = models.CharField(max_length=100,null=True, blank=True)
+    pub4 = models.CharField(max_length=100,null=True, blank=True)
+    pub5 = models.CharField(max_length=100,null=True, blank=True)
