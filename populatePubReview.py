49f79fdca5d93991e65f0018c40144824a6f5a4f
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2024-03-21 13:33:26 +0000

    path fixing

--- populatePubReview.py
+++ populatePubReview.py
@@ -99,7 +99,7 @@ def create_user_profile(username, password, email, first_name, last_name, image,
     profile = UserProfile.objects.get_or_create(user=userInstance, firstName=first_name, lastName=last_name, sex=sex, age=age, nationality=nationality)[0]
     
     # get path to pub image files
-    image_dir = os.path.join(settings.MEDIA_DIR, "/population_file_pictures/profile_images/")
+    image_dir = os.path.join(settings.STATIC_DIR, "population_file_images/profile_images")
 
     with open(os.path.join(image_dir, image), 'rb') as f:
         # assign image to Profile.picture field
@@ -116,7 +116,7 @@ def create_pub(owner, pub_name, city, street_name, postcode, image, ownerInstanc
     pub = Pub.objects.get_or_create(owner=ownerInstance, pubName=pub_name, city=city, streetName=street_name, postcode=postcode)[0]
 
     # get path to pub image files
-    image_dir = os.path.join(settings.MEDIA_DIR, "/population_pictures/pub_images")
+    image_dir = os.path.join(settings.STATIC_DIR, "population_file_images/pub_images")
 
     with open(os.path.join(image_dir, image), 'rb') as f:
         # assign image to Pub.picture field
